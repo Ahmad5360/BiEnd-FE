d02c91ae408f8dc08d0467219d711fa073baa5fe
Ahmad Rashid <ar53605360@gmail.com>
2024-04-27 20:10:06 +0500

    Added font and layout

---
+++ src/app/common/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='py-6'>
+      <div>
+        <p className='text-3xl font-medium'>BiEnd</p>
+      </div>
+    </div>
+  )
+}
+
+export default Header
