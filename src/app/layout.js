d3404c788b3cc4a30162a7d6548eca05664814c6
Ahmad Rashid <ar53605360@gmail.com>
2024-05-25 11:01:29 +0500

    Added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.png" sizes="any" />
       <body className={`${montserrat.className}`}>
         <Header />
         <div className="w-[calc(100%-50px)] mx-auto">{children}</div>
